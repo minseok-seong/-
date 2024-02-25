d4c183fa84b04829de26f1692c37bebd5399c23c
minseok <imuchgabis@gmail.com>
2024-02-25 18:57:16 +0900

    후..

--- 섹션3/가장 짧은 문자거리.js
+++ 섹션3/가장 짧은 문자거리.js
@@ -1,7 +1,9 @@
 function solution(str1, str2) {
+  let arr = str1.split("").map((a) => a.charCodeAt());
   let str2n = str2.charCodeAt();
+  let result = arr.map((a) => Math.abs(a - str2n));
 
-  console.log(+result);
+  console.log(result.join(" "));
 }
 
 solution("teachermode", "e");
---
+++ 섹션3/문자열 압축.js
@@ -0,0 +1,22 @@
+function solution(str) {
+  let result = {};
+  let str1 = "";
+  for (let i = 0; i < str.length; i++) {
+    if (!result[str[i]]) {
+      result[str[i]] = 1;
+    } else {
+      result[str[i]] += 1;
+    }
+  }
+  console.log(result);
+  for (k in result) {
+    if (result[k] === 1) {
+      str1 += k;
+    } else {
+      str1 += k + result[k];
+    }
+  }
+  console.log(str1);
+}
+
+solution("KKHSSSSSSSE");
