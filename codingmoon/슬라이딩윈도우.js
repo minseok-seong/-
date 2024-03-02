f67f2fea47361626778d2345fee89bb5449f2ffb
minseok <imuchgabis@gmail.com>
2024-03-02 21:05:22 +0900

    ps개념

---
+++ codingmoon/슬라이딩윈도우.js
@@ -0,0 +1,15 @@
+// maxSumOfSubArr문제;
+
+function maxSumOfSubArrq(arr, k) {
+  let windowSum = 0;
+  let maxSum = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    windowSum += arr[i];
+    if (i >= k - 1) {
+      maxSum = Math.max(maxSum, windowSum);
+      windowSum -= arr[i - (k - 1)];
+    }
+  }
+  return maxSum;
+}
