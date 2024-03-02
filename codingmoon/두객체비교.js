a4abc8a26b412b51eb044f6242e73d0f4e474f1d
minseok <imuchgabis@gmail.com>
2024-03-02 18:33:45 +0900

    객체비교

---
+++ codingmoon/두객체비교.js
@@ -0,0 +1,28 @@
+//객체는 참조타입이라 직접 비교불가
+
+const obj1 = {
+  a: "a",
+  b: 1,
+  c: ["a", "b", "c"],
+  d: {
+    e: null,
+    f: -1,
+  },
+};
+
+const obj2 = {
+  a: "a",
+  b: 1,
+  c: ["a", "b", "c"],
+  d: {
+    e: null,
+    f: -1,
+  },
+};
+
+function tets(obj1, obj2) {
+  let a = JSON.stringify(obj1);
+  let b = JSON.stringify(obj2);
+
+  return a.split("").sort().join("") === b.split("").sort().join("");
+}
