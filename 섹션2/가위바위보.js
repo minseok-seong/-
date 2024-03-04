030e5459a92c85182d388b66d99cce1073cdfc48
minseok <imuchgabis@gmail.com>
2024-03-04 15:46:27 +0900

    섹션2강의완

--- 섹션2/가위바위보.js
+++ 섹션2/가위바위보.js
@@ -31,3 +31,21 @@ function solution(arr1, arr2) {
 }
 
 solution([2, 3, 3, 1, 3], [1, 1, 2, 2, 3]);
+
+function solution(a, b) {
+  let answer = "";
+  for (let i = 0; i < a.length; i++) {
+    //누가 이기는지에 집중해라 무승부 케이스는 단하나 a가 이기는 경우만 체크하면 나머진 else 로 처리
+    if (a[i] === b[i]) answer += "D ";
+    else if (a[i] === 1 && b[i] === 3) answer += "A ";
+    else if (a[i] === 2 && b[i] === 1) answer += "A ";
+    else if (a[i] === 3 && b[i] === 2) answer += "A ";
+    else answer += "B ";
+  }
+
+  return answer;
+}
+
+let a = [2, 3, 3, 1, 3];
+let b = [1, 1, 2, 2, 3];
+console.log(solution(a, b));
