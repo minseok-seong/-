7c33afb2c79e883342806fbe01632ec4d10783cf
minseok <imuchgabis@gmail.com>
2024-03-04 18:27:38 +0900

    섹5강의완

--- 섹션5/두 배열 합치기.js
+++ 섹션5/두 배열 합치기.js
@@ -4,3 +4,22 @@ function solution(arr1, arr2) {
   console.log(arr);
 }
 solution([1, 3, 5], [2, 3, 6, 7, 9]);
+
+function solution(arr1, arr2) {
+  let answer = [];
+  let n = arr1.length;
+  let m = arr2.length;
+  let p1 = (p2 = 0);
+  while (p1 < n && p2 < m) {
+    if (arr1[p1] <= arr2[p2])
+      answer.push(arr1[p1++]); //값을 푸시하고 인덱스 증가
+    else answer.push(arr2[p2++]);
+  }
+  while (p1 < n) answer.push(arr1[p1++]);
+  while (p2 < m) answer.push(arr2[p2++]);
+  return answer;
+}
+
+let a = [1, 3, 5];
+let b = [2, 3, 6, 7, 9];
+console.log(solution(a, b)); //투포인터 알고리즘 방식
--- 섹션5/최대매출.js
+++ 섹션5/최대매출.js
@@ -1,11 +1,16 @@
 function solution(k, arr) {
-  let result = [];
+  let max = 0;
+  let hap = 0;
   for (let i = 0; i <= arr.length - k; i++) {
-    result.push(arr.slice(i, i + k).reduce((acc, v) => acc + v, 0));
+    hap += arr[i];
+
+    if (i >= k - 1) {
+      if (max < hap) max = hap;
+      hap -= arr[i - (k - 1)];
+    }
   }
-  console.log(result);
-  return Math.max(...result);
+  console.log(max);
 }
 
 let a = [12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
-console.log(solution(3, a));
+console.log(solution(3, a)); //슬라이딩 윈도우
