2f8882e054fd45b66fd70b1738f45598387a9565
minseok <imuchgabis@gmail.com>
2024-03-18 16:30:02 +0900

    dp

---
+++ 섹션10/계단오르기.js
@@ -0,0 +1,13 @@
+function solution(n) {
+  const dp = new Array(n).fill(0);
+  dp[0] = 0;
+  dp[1] = 1;
+  dp[2] = 2;
+  for (let i = 3; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+
+  console.log(dp[n]);
+}
+
+console.log(solution(7));
